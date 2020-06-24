127e0a5fb0ef5c11f16880852779fee79c921b30
Joseph Miller <joem@breachintelligence.com>
2020-06-24 10:15:38 -0400

    commiting intial integration for PR

---
+++ config/config.js
@@ -0,0 +1,106 @@
+module.exports = {
+  /**
+   * Name of the integration which is displayed in the Polarity integrations user interface
+   *
+   * @type String
+   * @required
+   */
+  name: 'Cofense Intelligence',
+  /**
+   * The acronym that appears in the notification window when information from this integration
+   * is displayed.  Note that the acronym is included as part of each "tag" in the summary information
+   * for the integration.  As a result, it is best to keep it to 4 or less characters.  The casing used
+   * here will be carried forward into the notification window.
+   *
+   * @type String
+   * @required
+   */
+  acronym: 'COF',
+  onDemandOnly: false,
+  /**
+   * Description for this integration which is displayed in the Polarity integrations user interface
+   *
+   * @type String
+   * @optional
+   */
+  description:
+    'Cofense Intelligence provides accurate and timely alerts to strengthen your organization’s ability to quickly identify and respond to phishing attacks in progress',
+  entityTypes: ['hash', 'ipv4', 'domain'],
+  /**
+   * Provide custom component logic and template for rendering the integration details block.  If you do not
+   * provide a custom template and/or component then the integration will display data as a table of key value
+   * pairs.
+   *
+   * @type Object
+   * @optional
+   */
+  styles: ['./styles/style.less'],
+  block: {
+    component: {
+      file: './components/block.js'
+    },
+    template: {
+      file: './templates/block.hbs'
+    }
+  },
+  summary: {
+    component: {
+      file: './components/summary.js'
+    },
+    template: {
+      file: './templates/summary.hbs'
+    }
+  },
+  request: {
+    // Provide the path to your certFile. Leave an empty string to ignore this option.
+    // Relative paths are relative to the integration's root directory
+    cert: '',
+    // Provide the path to your private key. Leave an empty string to ignore this option.
+    // Relative paths are relative to the integration's root directory
+    key: '',
+    // Provide the key passphrase if required.  Leave an empty string to ignore this option.
+    // Relative paths are relative to the integration's root directory
+    passphrase: '',
+    // Provide the Certificate Authority. Leave an empty string to ignore this option.
+    // Relative paths are relative to the integration's root directory
+    ca: '',
+    // An HTTP proxy to be used. Supports proxy Auth with Basic Auth, identical to support for
+    // the url parameter (by embedding the auth info in the uri)
+    proxy: '',
+
+    rejectUnauthorized: false
+  },
+  logging: {
+    level: 'trace' //trace, debug, info, warn, error, fatal
+  },
+  options: [
+    {
+      key: "url",
+      name: "Base URL for the Cofense Intelligence REST API",
+      description:
+        "The base URL for the Cofense Intelligence REST API including the schema (i.e., https://)",
+      type: "text",
+      default: "https://www.threathq.com",
+      userCanEdit: false,
+      adminOnly: true
+    },
+    {
+      key: 'apiUser',
+      name: 'API Username',
+      description: 'Valid Cofense Intelligence API Username',
+      default: '',
+      type: 'text',
+      userCanEdit: true,
+      adminOnly: false
+    },
+    {
+      key: 'apiPass',
+      name: 'API Password',
+      description: 'Valid Cofense Intelligence API Password associated with the specified Username',
+      default: '',
+      type: 'password',
+      userCanEdit: true,
+      adminOnly: false
+    }
+  ]
+};
---
+++ integration.js
@@ -0,0 +1,192 @@
+'use strict';
+
+const request = require('request');
+const config = require('./config/config');
+const async = require('async');
+const fs = require('fs');
+
+let Logger;
+let requestWithDefaults;
+
+const MAX_PARALLEL_LOOKUPS = 10;
+
+function startup(logger) {
+  let defaults = {};
+  Logger = logger;
+
+  const { cert, key, passphrase, ca, proxy, rejectUnauthorized } = config.request;
+
+  if (typeof cert === 'string' && cert.length > 0) {
+    defaults.cert = fs.readFileSync(cert);
+  }
+
+  if (typeof key === 'string' && key.length > 0) {
+    defaults.key = fs.readFileSync(key);
+  }
+
+  if (typeof passphrase === 'string' && passphrase.length > 0) {
+    defaults.passphrase = passphrase;
+  }
+
+  if (typeof ca === 'string' && ca.length > 0) {
+    defaults.ca = fs.readFileSync(ca);
+  }
+
+  if (typeof proxy === 'string' && proxy.length > 0) {
+    defaults.proxy = proxy;
+  }
+
+  if (typeof rejectUnauthorized === 'boolean') {
+    defaults.rejectUnauthorized = rejectUnauthorized;
+  }
+
+  requestWithDefaults = request.defaults(defaults);
+}
+
+function doLookup(entities, options, cb) {
+  let lookupResults = [];
+  let tasks = [];
+
+  Logger.debug(entities);
+  entities.forEach((entity) => {
+    let requestOptions = {
+      method: 'POST',
+      auth: {
+        user: options.apiUser,
+        pass: options.apiPass
+      },
+      uri: `${options.url}/apiv1/threat/search`,
+      json: true
+    };
+
+    if (entity.isHash) {
+      requestOptions.qs = {
+        allHash: `${entity.value}`
+      };
+    } else if (entity.isIPv4) {
+      requestOptions.qs = {
+        ip: `${entity.value}`
+      };
+    } else if (entity.isDomain) {
+      requestOptions.qs = {
+        domain: `${entity.value}`
+      };
+    } else {
+      return;
+    }
+
+    Logger.trace({ requestOptions }, 'Request Options');
+
+    tasks.push(function(done) {
+      requestWithDefaults(requestOptions, function(error, res, body) {
+        Logger.trace({ body, status: res.statusCode });
+        let processedResult = handleRestError(error, entity, res, body);
+
+        if (processedResult.error) {
+          done(processedResult);
+          return;
+        }
+
+        done(null, processedResult);
+      });
+    });
+  });
+
+  async.parallelLimit(tasks, MAX_PARALLEL_LOOKUPS, (err, results) => {
+    if (err) {
+      Logger.error({ err: err }, 'Error');
+      cb(err);
+      return;
+    }
+
+    results.forEach((result) => {
+      if (result.body === null || result.body.length === 0 || result.body.data.page.totalElements === 0) {
+        lookupResults.push({
+          entity: result.entity,
+          data: null
+        });
+      } else {
+        lookupResults.push({
+          entity: result.entity,
+          data: {
+            summary: [],
+            details: result.body
+          }
+        });
+      }
+    });
+
+    Logger.debug({ lookupResults }, 'Results');
+    cb(null, lookupResults);
+  });
+}
+
+function handleRestError(error, entity, res, body) {
+  let result;
+
+  if (error) {
+    return {
+      error: error,
+      detail: 'HTTP Request Error'
+    };
+  }
+
+  if (res.statusCode === 200 && body) {
+    // we got data!
+    result = {
+      entity: entity,
+      body: body
+    };
+  } else if (res.statusCode === 400) {
+    result = {
+      error: 'Bad Request',
+      detail: "Request was invalid"
+    };
+  } else if (res.statusCode === 401) {
+    result = {
+      error: 'Unauthorized',
+      detail: "Unauthorized, please check your credentials"
+    };
+  } else if (res.statusCode === 404) {
+    result = {
+      error: 'Not Found',
+      detail: "Query resulted in not found"
+    };
+  } else if (Math.round(statusCode / 10) * 10 === 500) {
+    result = {
+      error: body,
+      statusCode: res ? res.statusCode : 'Unknown',
+      detail: 'An unexpected error occurred'
+    };
+  }
+
+  return result;
+}
+
+function validateOption(errors, options, optionName, errMessage) {
+  if (
+    typeof options[optionName].value !== 'string' ||
+    (typeof options[optionName].value === 'string' && options[optionName].value.length === 0)
+  ) {
+    errors.push({
+      key: optionName,
+      message: errMessage
+    });
+  }
+}
+
+function validateOptions(options, callback) {
+  let errors = [];
+
+  validateOption(errors, options, 'url', 'You must provide a valid URL.');
+  validateOption(errors, options, 'apiUser', 'You must provide a valid API username.');
+  validateOption(errors, options, 'apiPass', 'You must provide a valid API password.');
+
+  callback(null, errors);
+}
+
+module.exports = {
+  doLookup: doLookup,
+  validateOptions: validateOptions,
+  startup: startup
+};
